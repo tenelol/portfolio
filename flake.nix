ec598ddaef50ae32ec60d1e086ecd78b471a8cd7
tener <tenelol@tenelol.dev>
2026-01-11 20:18:26 +0900

    Add nix run app for dev server

--- flake.nix
+++ flake.nix
@@ -18,6 +18,18 @@
         pkgs = import nixpkgs { inherit system; };
       in
       {
+        apps.default = flake-utils.lib.mkApp {
+          drv = pkgs.writeShellApplication {
+            name = "devserver";
+            runtimeInputs = [
+              pkgs.nodejs_20
+              pkgs.nodePackages.pnpm
+            ];
+            text = ''
+              exec pnpm dev
+            '';
+          };
+        };
         devShells.default = pkgs.mkShell {
           packages = [
             pkgs.nodejs_20
