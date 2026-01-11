1982b9baf5ddbe206c822b592a79630decf2c152
tener <tenelol@tenelol.dev>
2026-01-11 19:53:27 +0900

    Add Nix flake files

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "portfolio";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = [
+            pkgs.nodejs_20
+            pkgs.nodePackages.pnpm
+          ];
+        };
+      }
+    );
+}
